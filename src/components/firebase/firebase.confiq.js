20069a9dd64786ed45ebb3680e67a648e9731bb0
infan11 <infanjiounrahman20606@gmail.com>
2023-12-28 18:46:21 +0600

    added firebase.config.js

--- src/components/firebase/firebase.confiq.js
+++ src/components/firebase/firebase.confiq.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDz9fuW2JdQh-BgGmiMuHDvfc639Labjxk",
-  authDomain: "brand-shop-client-1bdb1.firebaseapp.com",
-  projectId: "brand-shop-client-1bdb1",
-  storageBucket: "brand-shop-client-1bdb1.appspot.com",
-  messagingSenderId: "1026925161506",
-  appId: "1:1026925161506:web:f96bdfd90cc18373a6ee7b"
+const firebaseConfig = { 
+  // 
+  apiKey: import.meta.env.VITE_KEYapiKey,
+  authDomain: import.meta.env.VITE_KEYauthDomain,
+  projectId: import.meta.env. VITE_KEYprojectId,
+  storageBucket: import.meta.env.VITE_KEYstorageBucket,
+  messagingSenderId: import.meta.env.VITE_KEYmessagingSenderId,
+  appId: import.meta.env.VITE_KEYappId
 };
 
 // Initialize Firebase
